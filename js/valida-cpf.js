4b614504443c89dacdc0c959cead91ad999163a8
Stefani Dal Puppo <stefanipuppo@hotmail.com>
2024-04-12 15:53:21 -0300

    comunicação com o usuario e erros

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,30 +1,26 @@
 export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
     if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
-        console.log("Esse cpf não existe!")
-
-    } else {
-        console.log("Existe!")
+        campo.setCustomValidity('Esse cpf não é válido')
     }
 }
 
 function validaNumerosRepetidos(cpf) {
     const numerosRepetidos = [
-    '00000000000',
-    '11111111111',
-    '22222222222',
-    '33333333333',
-    '44444444444',
-    '55555555555',
-    '66666666666',
-    '77777777777',
-    '88888888888',
-    '99999999999'
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
     ]
 
-    return numerosRepetidos.includes(cpf);
+    return numerosRepetidos.includes(cpf)
 }
-
 
 function validaPrimeiroDigito(cpf) {
     let soma = 0;
